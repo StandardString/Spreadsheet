4680b290b6aba675d03d324a48ad761b3cabdcc1
Austin Nester <nester@lab1-37.eng.utah.edu>
2013-04-11 15:04:02 -0600

    Testing access from another computer

--- TestServer/client.cpp
+++ TestServer/client.cpp
@@ -1,7 +1,7 @@
 //
 // client.cpp
 // ~~~~~~~~~~
-//
+//DICKSON IS A HARLOT.
 // Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
